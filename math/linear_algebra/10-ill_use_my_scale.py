1249778043144be2f0c7736340ee9886b616cc7c
davidbirenzi <d.birenzi@alustudent.com>
2025-09-14 22:58:20 +0300

    corrected some mistakes

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 def np_shape(matrix):
     """Returns the shape of a numpy ndarray as a tuple"""
-    return matrix.shape
+    return matrix.shape # Using the .shape attribute of numpy ndarray
--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -2,4 +2,5 @@
 
 def np_transpose(matrix):
     """Returns the transpose of a numpy.ndarray without importing numpy"""
-    return matrix.T
+    return matrix.T # Using the .T attribute of numpy ndarray
+
