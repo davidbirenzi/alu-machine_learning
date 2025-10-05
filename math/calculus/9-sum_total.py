136f724fdd1d844b73bff8df9d66600bc1757b37
davidbirenzi <d.birenzi@alustudent.com>
2025-10-05 18:37:43 +0300

    corrected number 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
-'''
-    The function below calculates
-    the integral of a polynomial
-'''
-9
+"""
+This module provides a function to calculate,
+the sum of squares,
+of the first n natural numbers.
+"""
+
+
+def summation_i_squared(n):
+    """
+    Calculate the sum of squares of the first n natural numbers.
+    """
+    if not isinstance(n, int) or n < 1:
+        return None
+    return n * (n + 1) * (2 * n + 1) // 6
